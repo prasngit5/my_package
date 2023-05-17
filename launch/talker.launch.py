94c5ee81fbf6a6e421a725d942c7245fb3574260
pras_n <prasngit5@gmail.com>
2023-05-17 13:19:42 -0500

    first commit

---
+++ launch/talker.launch.py
@@ -0,0 +1,11 @@
+from launch import launchDescription
+from launch_ros.actions import Node
+
+def generate_launch_description():
+
+    return launchDescription([
+        Node(
+        package='demo_node_cpp',
+        executable='talker'
+        )
+    ])
